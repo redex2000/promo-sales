fd1fcd2d4f1c67eb7190c90a32b2cc8d96773b5b
Artur Galyamov <redex2000@gmail.com>
2016-03-10 23:16:58 +0500

    Добавлена проверка уникальности промокода refs #2328

--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -5,4 +5,5 @@ class PromoCode < ActiveRecord::Base
   validates :code, :discount_sum, :count, presence: true
   validates :discount_sum, numericality: {greater_than_or_equal_to: 0}
   validates :count, numericality: {greater_than_or_equal_to: -1}
+  validates :code, uniqueness: true
 end
