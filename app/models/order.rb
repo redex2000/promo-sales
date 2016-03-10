d7a6d60654118689ac60deec930dbde52d0580be
Artur Galyamov <redex2000@gmail.com>
2016-03-10 23:15:51 +0500

    Изменена немного валидация для заказов refs #2327

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,5 @@ class Order < ActiveRecord::Base
 
   validates :description, :cost, :ip, presence: true
   validates :cost, numericality: { greater_than: 0 }
-  validates :ip, length: 15
-  validates :promo_code, uniqueness: true
+  validates :ip, length: {in: 7..15}
 end
