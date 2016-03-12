de61216cc72a8678e7a05213439899d7c6cd0682
Artur Galyamov <redex2000@gmail.com>
2016-03-12 23:47:28 +0500

    Реализована простейшая вёрстка с помощью bootstrap closes #2338

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'slim'
 gem 'slim-rails'
 
+# Используем boostrap
+gem 'bootstrap-sass'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module PromoSales
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Обеспечиваем связь bootstrap с файлопроводом
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
