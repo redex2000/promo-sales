0dbc2e160817511fb8ccd6833e6f7f07aec936ca
Artur Galyamov <redex2000@gmail.com>
2016-03-14 15:01:25 +0500

    Теперь в админке выводится итоговая сумма closes #2337

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,10 @@
 module OrdersHelper
+  def show_cost(order)
+    if order.promo_code.nil?
+      cost = order.cost
+    else
+      cost = order.promo_code.calculate_total_cost order.cost
+    end
+    cost.to_s + ' roubles'
+  end
 end
